c4f37872b8afb72c91cad5326d3adb3c4355625b
NotADev9000 <NotADev9000@users.noreply.github.com>
2024-12-10 15:09:13 +0000

    Fix pet look bugs

--- Assets/Scripts/States/State_ReadyToPlay.cs
+++ Assets/Scripts/States/State_ReadyToPlay.cs
@@ -3,7 +3,7 @@ public static class State_ReadyToPlay
     public static float RecalcPathTime = 0.3f;
     public static float RecalcPathTimer = 0f;
 
-    public static float PetImpatientMinTime = 5f;
+    public static float PetImpatientMinTime = 3f;
     public static float PetImpatientMaxTime = 10f;
     public static float PetImpatientCooldownTime = 15f;
     public static float PetImpatientTimer = 0f;
